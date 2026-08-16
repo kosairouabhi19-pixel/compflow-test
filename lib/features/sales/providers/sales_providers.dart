64ff7a2885add5ddf951c7fb386a743ce36738ec
kosairouabhi19-pixel <kosai.rouabhi19@gmail.com>
2026-08-16 03:35:20 +0100

    refactor(sales): use shared AppDatabase provider

--- lib/features/sales/providers/sales_providers.dart
+++ lib/features/sales/providers/sales_providers.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../products/providers/products_providers.dart';
+import '../../../core/database/database_provider.dart';
 import '../database/sales_dao.dart';
 import '../database/sale_items_dao.dart';
 import '../repositories/sales_repository.dart';
