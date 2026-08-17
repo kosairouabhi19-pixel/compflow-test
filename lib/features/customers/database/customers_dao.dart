e54e3ee95cff6854bc4b6449d7097d9c4be9582c
kosairouabhi19-pixel <kosai.rouabhi19@gmail.com>
2026-08-17 03:41:54 +0100

    Secure customer DAO with tenant-scoped CRUD

--- lib/features/customers/database/customers_dao.dart
+++ lib/features/customers/database/customers_dao.dart
@@ -8,36 +8,60 @@ part 'customers_dao.g.dart';
 @DriftAccessor(tables: [Customers])
 class CustomersDao extends DatabaseAccessor<AppDatabase>
     with _$CustomersDaoMixin {
-  CustomersDao(super.db);
+  CustomersDao(super.db, this.tenantId);
+
+  final String tenantId;
 
   Future<List<Customer>> getAllCustomers() {
-    return select(customers).get();
+    return (select(customers)
+          ..where((t) => t.tenantId.equals(tenantId) & t.deletedAt.isNull()))
+        .get();
   }
 
   Stream<List<Customer>> watchAllCustomers() {
-    return select(customers).watch();
+    return (select(customers)
+          ..where((t) => t.tenantId.equals(tenantId) & t.deletedAt.isNull()))
+        .watch();
   }
 
   Future<Customer?> getCustomerById(String id) {
-    return (select(customers)..where((t) => t.id.equals(id)))
+    return (select(customers)
+          ..where((t) =>
+              t.id.equals(id) &
+              t.tenantId.equals(tenantId) &
+              t.deletedAt.isNull()))
         .getSingleOrNull();
   }
 
   Future<int> insertCustomer(CustomersCompanion customer) {
-    return into(customers).insert(customer);
+    return into(customers).insert(customer.copyWith(tenantId: Value(tenantId)));
   }
 
   Future<bool> updateCustomer(Customer customer) {
-    return update(customers).replace(customer);
+    if (customer.tenantId != tenantId || customer.deletedAt != null) {
+      return Future.value(false);
+    }
+
+    return (update(customers)
+          ..where((t) => t.id.equals(customer.id) & t.tenantId.equals(tenantId)))
+        .write(customer);
   }
 
-  Future<int> deleteCustomer(String id) {
-    return (delete(customers)..where((t) => t.id.equals(id))).go();
+  Future<int> softDeleteCustomer(String id) {
+    return (update(customers)
+          ..where((t) => t.id.equals(id) & t.tenantId.equals(tenantId)))
+        .write(CustomersCompanion(
+          deletedAt: Value(DateTime.now()),
+          updatedAt: Value(DateTime.now()),
+        ));
   }
 
   Future<List<Customer>> searchCustomers(String query) {
     return (select(customers)
-          ..where((t) => t.fullName.like('%$query%')))
+          ..where((t) =>
+              t.tenantId.equals(tenantId) &
+              t.deletedAt.isNull() &
+              (t.fullName.like('%$query%') | t.phone.like('%$query%'))))
         .get();
   }
 }
