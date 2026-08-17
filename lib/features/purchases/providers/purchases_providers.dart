f2b085eec8af024aa1fd8ac4f50945d11139adee
kosairouabhi19-pixel <kosai.rouabhi19@gmail.com>
2026-08-17 03:54:50 +0100

    Use current tenant for purchases

--- lib/features/purchases/providers/purchases_providers.dart
+++ lib/features/purchases/providers/purchases_providers.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/database_provider.dart';
+import '../../../core/providers/current_tenant_provider.dart';
 import '../database/purchases_dao.dart';
 import '../repositories/purchases_repository.dart';
 
 final purchasesDaoProvider = Provider<PurchasesDao>((ref) {
-  return PurchasesDao(ref.watch(appDatabaseProvider));
+  final tenantId = ref.watch(currentTenantProvider);
+  if (tenantId == null) {
+    throw StateError('لا يوجد متجر حالي للمستخدم');
+  }
+  return PurchasesDao(ref.watch(appDatabaseProvider), tenantId);
 });
 
 final purchasesRepositoryProvider = Provider<PurchasesRepository>((ref) {
