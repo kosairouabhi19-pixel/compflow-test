89c814f86293da37e05f895291899498e2d1bf9f
kosairouabhi19-pixel <kosai.rouabhi19@gmail.com>
2026-08-17 03:36:28 +0100

    Inject authenticated tenant into product DAO

--- lib/features/products/providers/products_providers.dart
+++ lib/features/products/providers/products_providers.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/database_provider.dart';
+import '../../auth/providers/auth_providers.dart';
 import '../database/products_dao.dart';
 import '../repositories/products_repository.dart';
 
 final productsDaoProvider = Provider<ProductsDao>((ref) {
-  return ProductsDao(ref.watch(appDatabaseProvider));
+  final tenantId = ref.watch(authControllerProvider).user?.tenantId ?? '';
+  return ProductsDao(ref.watch(appDatabaseProvider), tenantId);
 });
 
 final productsRepositoryProvider = Provider<ProductsRepository>((ref) {
