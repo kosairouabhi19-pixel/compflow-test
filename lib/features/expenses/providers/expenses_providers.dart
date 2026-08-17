d41d46c83556f225d023701a084557d292431321
kosairouabhi19-pixel <kosai.rouabhi19@gmail.com>
2026-08-17 03:54:55 +0100

    Use current tenant for expenses

--- lib/features/expenses/providers/expenses_providers.dart
+++ lib/features/expenses/providers/expenses_providers.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/database_provider.dart';
+import '../../../core/providers/current_tenant_provider.dart';
 import '../database/expenses_dao.dart';
 import '../repositories/expenses_repository.dart';
 
 final expensesDaoProvider = Provider<ExpensesDao>((ref) {
-  return ExpensesDao(ref.watch(appDatabaseProvider));
+  final tenantId = ref.watch(currentTenantProvider);
+  if (tenantId == null) {
+    throw StateError('لا يوجد متجر حالي للمستخدم');
+  }
+  return ExpensesDao(ref.watch(appDatabaseProvider), tenantId);
 });
 
 final expensesRepositoryProvider = Provider<ExpensesRepository>((ref) {
