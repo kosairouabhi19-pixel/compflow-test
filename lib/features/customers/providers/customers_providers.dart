9e639a78ca20e9b520174b22fa1feae10385c898
kosairouabhi19-pixel <kosai.rouabhi19@gmail.com>
2026-08-17 03:41:59 +0100

    Inject authenticated tenant into customer DAO

--- lib/features/customers/providers/customers_providers.dart
+++ lib/features/customers/providers/customers_providers.dart
@@ -2,11 +2,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/app_database.dart';
 import '../../../core/database/database_provider.dart';
+import '../../auth/providers/auth_providers.dart';
 import '../database/customers_dao.dart';
 import '../repositories/customers_repository.dart';
 
 final customersDaoProvider = Provider<CustomersDao>((ref) {
-  return CustomersDao(ref.watch(appDatabaseProvider));
+  final tenantId = ref.watch(authControllerProvider).user?.tenantId ?? '';
+  return CustomersDao(ref.watch(appDatabaseProvider), tenantId);
 });
 
 final customersRepositoryProvider = Provider<CustomersRepository>((ref) {
