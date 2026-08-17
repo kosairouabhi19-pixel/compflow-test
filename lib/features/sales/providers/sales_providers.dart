f3159452b5a3c08919e3aca621803322291cf2dd
kosairouabhi19-pixel <kosai.rouabhi19@gmail.com>
2026-08-17 03:49:28 +0100

    Wire Sales DAOs to current tenant

--- lib/features/sales/providers/sales_providers.dart
+++ lib/features/sales/providers/sales_providers.dart
@@ -1,16 +1,25 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/database/database_provider.dart';
+import '../../../core/providers/current_tenant_provider.dart';
 import '../database/sales_dao.dart';
 import '../database/sale_items_dao.dart';
 import '../repositories/sales_repository.dart';
 
 final salesDaoProvider = Provider<SalesDao>((ref) {
-  return SalesDao(ref.watch(appDatabaseProvider));
+  final tenantId = ref.watch(currentTenantProvider);
+  if (tenantId == null) {
+    throw StateError('لا يمكن الوصول إلى المبيعات بدون متجر حالي');
+  }
+  return SalesDao(ref.watch(appDatabaseProvider), tenantId);
 });
 
 final saleItemsDaoProvider = Provider<SaleItemsDao>((ref) {
-  return SaleItemsDao(ref.watch(appDatabaseProvider));
+  final tenantId = ref.watch(currentTenantProvider);
+  if (tenantId == null) {
+    throw StateError('لا يمكن الوصول إلى عناصر المبيعات بدون متجر حالي');
+  }
+  return SaleItemsDao(ref.watch(appDatabaseProvider), tenantId);
 });
 
 final salesRepositoryProvider = Provider<SalesRepository>((ref) {
